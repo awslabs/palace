7f92a37c878d0a568cbfe44a0a6f7d747975f1f9
Hugh Carson <hughcars@amazon.com>
2023-03-03 18:23:07 -0500

    Return to more accurate FluxProjector filename

--- palace/linalg/fluxprojector.cpp
+++ palace/linalg/fluxprojector.cpp
@@ -1,7 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-#include "errorestimator.hpp"
+#include "fluxprojector.hpp"
 
 #include <limits>
 #include "fem/materialoperator.hpp"
