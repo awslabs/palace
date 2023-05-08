39acc55b1d39caa383a2f37b828ab25c843bcdbd
Sebastian Grimberg <sjg@amazon.com>
2023-05-08 11:55:31 -0700

    Add SLEPc as an independent Git submodule rather than using --download-slepc from PETSc
    Depends on https://gitlab.com/petsc/petsc/-/merge_requests/6434.

--- cmake/extern/ExternalGitTags.cmake
+++ cmake/extern/ExternalGitTags.cmake
@@ -11,11 +11,11 @@ endif()
 set(__extern_git_tags YES)
 
 set(EXTERN_ARPACK_GIT_TAG
-  "22172ff21222c92bb1360c39e8aa62747610a1dd" CACHE STRING  # 04/10/2023
+  "3e3e12fef2c516fe21658d2e67dc8cbe86ec60bb" CACHE STRING  # 05/25/2023
   "Git tag for external ARPACK-NG build"
 )
 set(EXTERN_BUTTERFLYPACK_GIT_TAG
-  "04d498f5ef170fade5fd9f3caed841354b215a5a" CACHE STRING  # 04/13/2023
+  "703d67848cc1901f3c827519e1780a1441fa49f6" CACHE STRING  # 05/09/2023
   "Git tag for external ButterflyPACK build"
 )
 set(EXTERN_GKLIB_GIT_TAG
@@ -27,15 +27,15 @@ set(EXTERN_GSLIB_GIT_TAG
   "Git tag for external GSLIB build"
 )
 set(EXTERN_HYPRE_GIT_TAG
-  "e351324df841fdb323e1791162915a55f1ba642a" CACHE STRING  # 05/02/2023
+  "2b2e9d2eee238a72ee15ae68b1f3d7e22b6d495e" CACHE STRING  # 05/19/2023
   "Git tag for external HYPRE build"
 )
 set(EXTERN_METIS_GIT_TAG
-  "e0f1b88b8efcb24ffa0ec55eabb78fbe61e58ae7" CACHE STRING  # 04/03/2022
+  "e0f1b88b8efcb24ffa0ec55eabb78fbe61e58ae7" CACHE STRING  # 04/03/2023
   "Git tag for external METIS build"
 )
 set(EXTERN_MUMPS_GIT_TAG
-  "69b17761914d7dc71de6d67e4594c4cf55bceb5f" CACHE STRING  # 05/02/2023
+  "dc37cf6e3413f75cb39e867c4b7d0ce09d02a4cd" CACHE STRING  # 05/04/2023
   "Git tag for external MUMPS build"
 )
 set(EXTERN_PARMETIS_GIT_TAG
@@ -43,23 +43,23 @@ set(EXTERN_PARMETIS_GIT_TAG
   "Git tag for external ParMETIS build"
 )
 set(EXTERN_PETSC_GIT_TAG
-  "c377bc1b91c268822f21b2f974c0bfcf48ec1990" CACHE STRING  # 05/02/2023
+  "6694d23023a85ac50ab9ebd4bb517715c165a429" CACHE STRING  # 05/25/2023
   "Git tag for external PETSc build"
-)
-set(EXTERN_SLEPC_GIT_TAG
-  "59bebb3495c16e27671f3799be79dd1fb541dd93" CACHE STRING  # 04/28/2023
-  "Git tag for external SLEPc build"
 )
 set(EXTERN_SCALAPACK_GIT_TAG
   "b24a040ce5d9f7d262cef223134bd12d372cd72f" CACHE STRING  # 02/22/2022
   "Git tag for external ScaLAPACK build"
 )
+set(EXTERN_SLEPC_GIT_TAG
+  "cc8b4002b6b025d3631b960c2f0fef89f469d199" CACHE STRING  # 05/23/2023
+  "Git tag for external SLEPc build"
+)
 set(EXTERN_STRUMPACK_GIT_TAG
-  "72d2236946ebb2e97e79faf6b90eaa1d7edd4769" CACHE STRING  # 05/02/2023
+  "ce9d0a3dc5a37d08b1bf96e3ce023e80088a2576" CACHE STRING  # 05/25/2023
   "Git tag for external STRUMPACK build"
 )
 set(EXTERN_SUPERLU_GIT_TAG
-  "09cb1430f7be288fd4d75b8ed461aa0b7e68fefe" CACHE STRING  # 05/02/2023
+  "02b7c0d71bc33e785d098b0f8e4c26414bb8e39a" CACHE STRING  # 05/08/2023
   "Git tag for external SuperLU_DIST build"
 )
 set(EXTERN_ZFP_GIT_TAG
