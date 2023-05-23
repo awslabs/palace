25b779c3d17e419443d5e8d7b6196673f8ef2bc0
Sebastian Grimberg <sjg@amazon.com>
2023-05-22 20:50:51 -0700

    WIP: Add CG, GMRES, and FGMRES solver implementations for both real- and complex-valued operators, as part of a new Solver<OperType> class

---
+++ palace/linalg/orthog.hpp
@@ -0,0 +1,63 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef PALACE_LINALG_ORTHOG_HPP
+#define PALACE_LINALG_ORTHOG_HPP
+
+#include <vector>
+#include "linalg/vector.hpp"
+#include "utils/communication.hpp"
+
+namespace palace::linalg
+{
+
+//
+// Orthogonalization functions for a orthogonalizing a vector against a number of basis
+// vectors using modified or classical Gram-Schmidt.
+//
+
+template <typename VecType, typename ScalarType>
+inline void OrthogonalizeColumnMGS(MPI_Comm comm, const std::vector<VecType> &V, VecType &w,
+                                   ScalarType *H, int m)
+{
+  MFEM_ASSERT(m <= V.size(), "Out of bounds number of columns for MGS orthogonalization!");
+  for (int j = 0; j < m; j++)
+  {
+    H[j] = linalg::Dot(comm, w, V[j]);  // Global inner product
+    w.Add(-H[j], V[j]);
+  }
+}
+
+template <typename VecType, typename ScalarType>
+inline void OrthogonalizeColumnCGS(MPI_Comm comm, const std::vector<VecType> &V, VecType &w,
+                                   ScalarType *H, int m, bool refine = false)
+{
+  MFEM_ASSERT(m <= V.size(), "Out of bounds number of columns for CGS orthogonalization!");
+  for (int j = 0; j < m; j++)
+  {
+    H[j] = w * V[j];  // Local inner product
+  }
+  Mpi::GlobalSum(m, H, comm);
+  for (int j = 0; j < m; j++)
+  {
+    w.Add(-H[j], V[j]);
+  }
+  if (refine)
+  {
+    std::vector<ScalarType> dH(m);
+    for (int j = 0; j < m; j++)
+    {
+      dH[j] = w * V[j];  // Local inner product
+    }
+    Mpi::GlobalSum(m, dH.data(), comm);
+    for (int j = 0; j < m; j++)
+    {
+      H[j] += dH[j];
+      w.Add(-dH[j], V[j]);
+    }
+  }
+}
+
+}  // namespace palace::linalg
+
+#endif  // PALACE_LINALG_ORTHOG_HPP
---
+++ palace/linalg/solver.cpp
@@ -0,0 +1,41 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+#include "solver.hpp"
+
+namespace palace
+{
+
+template <>
+void WrapperSolver<Operator>::SetOperator(const Operator &op)
+{
+  pc->SetOperator(op);
+}
+
+template <>
+void WrapperSolver<ComplexOperator>::SetOperator(const ComplexOperator &op)
+{
+  MFEM_VERIFY(op.IsReal() && op.HasReal(),
+              "WrapperSolver::SetOperator assumes an operator which is purely real!");
+  pc->SetOperator(*op.Real());
+}
+
+template <>
+void WrapperSolver<Operator>::Mult(const Vector &x, Vector &y) const
+{
+  pc->Mult(x, y);
+}
+
+template <>
+void WrapperSolver<ComplexOperator>::Mult(const ComplexVector &x, ComplexVector &y) const
+{
+  mfem::Array<const Vector *> X(2);
+  mfem::Array<Vector *> Y(2);
+  X[0] = &x.Real();
+  X[1] = &x.Imag();
+  Y[0] = &y.Real();
+  Y[1] = &y.Imag();
+  pc->ArrayMult(X, Y);
+}
+
+}  // namespace palace
