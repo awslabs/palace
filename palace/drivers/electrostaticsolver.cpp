a0332a139ce83a03c8f47cd4ff74bbb921e20b99
Hugh Carson <hughcars@amazon.com>
2023-07-25 18:25:23 -0400

    Upgrade the GradFluxErrorEstimator to perform the projection component wise

--- palace/drivers/electrostaticsolver.cpp
+++ palace/drivers/electrostaticsolver.cpp
@@ -85,7 +85,6 @@ ElectrostaticSolver::Solve(const std::vector<std::unique_ptr<mfem::ParMesh>> &me
   };
 
   // Compute and reduce the error indicators for each solution.
-  // TODO: Possible to treat this more efficiently by solving with multiple RHS.
   std::for_each(V.begin(), V.end(), update_error_indicators);
 
   // Register the indicator field used to drive the overall adaptation.
