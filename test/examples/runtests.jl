8610bbd880afcaf4edd57022b207a529487a74e2
Sebastian Grimberg <sjg@amazon.com>
2023-11-09 15:28:58 -0800

    Update regression testing framework to add simple helper for rebaselining

--- test/examples/runtests.jl
+++ test/examples/runtests.jl
@@ -6,144 +6,181 @@ using Hwloc
 include("testcase.jl")
 
 if "PALACE_TEST" in keys(ENV)
-    palace = split(ENV["PALACE_TEST"], ' ')
+    palace = String.(split(ENV["PALACE_TEST"], ' '))
 else
     palace = "palace"
 end
 
 if "NUM_PROC_TEST" in keys(ENV)
-    numprocs = ENV["NUM_PROC_TEST"]
+    numprocs = parse(Int, ENV["NUM_PROC_TEST"])
 else
     numprocs = num_physical_cores()
 end
 
+if "TEST_CASES" in keys(ENV)
+    cases = String.(split(ENV["TEST_CASES"], ' '))
+else
+    cases = [
+        "spheres",
+        "rings",
+        "cavity/pec",
+        "cavity/impedance",
+        "coaxial/open",
+        "coaxial/matched",
+        "cpw/lumped_uniform",
+        "cpw/wave_uniform",
+        "cpw/lumped_adaptive",
+        "cpw/wave_adaptive"
+    ]
+end
+
+@info "Starting regression tests using `$palace` on $numprocs core$(numprocs > 1 ? "s" : "")"
+
 reltol = 1.0e-4
 abstol = 1.0e-16
 
-@info "Starting regression tests using `$palace` on $numprocs cores"
+if "spheres" in cases
+    @info "Testing spheres..."
+    @time testcase(
+        "spheres",
+        "spheres.json",
+        "";
+        palace=palace,
+        np=numprocs,
+        rtol=reltol,
+        atol=abstol,
+        excluded_columns=["Maximum", "Minimum"]
+    )
+end
 
-@info "Testing spheres..."
-@time testcase(
-    "spheres",
-    "spheres.json",
-    "";
-    palace=palace,
-    np=numprocs,
-    rtol=reltol,
-    atol=abstol,
-    excluded_columns=["Maximum", "Minimum"]
-)
+if "rings" in cases
+    @info "Testing rings..."
+    @time testcase(
+        "rings",
+        "rings.json",
+        "";
+        palace=palace,
+        np=numprocs,
+        rtol=reltol,
+        atol=abstol,
+        excluded_columns=["Maximum", "Minimum"]
+    )
+end
 
-@info "Testing rings..."
-@time testcase(
-    "rings",
-    "rings.json",
-    "";
-    palace=palace,
-    np=numprocs,
-    rtol=reltol,
-    atol=abstol,
-    excluded_columns=["Maximum", "Minimum"]
-)
+if "cavity/pec" in cases
+    @info "Testing cavity (PEC)..."
+    @time testcase(
+        "cavity",
+        "cavity_pec.json",
+        "pec";
+        palace=palace,
+        np=numprocs,
+        rtol=reltol,
+        atol=abstol,
+        excluded_columns=["Maximum", "Minimum", "Error (Bkwd.)", "Error (Abs.)"],
+        skip_rowcount=true
+    )
+end
 
-@info "Testing cavity (PEC)..."
-@time testcase(
-    "cavity",
-    "cavity_pec.json",
-    "pec";
-    palace=palace,
-    np=numprocs,
-    rtol=reltol,
-    atol=abstol,
-    excluded_columns=["Maximum", "Minimum"],
-    skip_rowcount=true
-)
-
-@info "Testing cavity (impedance)..."
-@time testcase(
-    "cavity",
-    "cavity_impedance.json",
-    "impedance";
-    palace=palace,
-    np=numprocs,
-    rtol=reltol,
-    atol=abstol,
-    excluded_columns=["Maximum", "Minimum"],
-    skip_rowcount=true
-)
+if "cavity/impedance" in cases
+    @info "Testing cavity (impedance)..."
+    @time testcase(
+        "cavity",
+        "cavity_impedance.json",
+        "impedance";
+        palace=palace,
+        np=numprocs,
+        rtol=reltol,
+        atol=abstol,
+        excluded_columns=["Maximum", "Minimum", "Error (Bkwd.)", "Error (Abs.)"],
+        skip_rowcount=true
+    )
+end
 
 # Coarser test tolerances for driven simulations with ports
 reltol = 2.0e-2
 abstol = 2.0e-12
 
-@info "Testing coaxial (open)..."
-@time testcase(
-    "coaxial",
-    "coaxial_open.json",
-    "open";
-    palace=palace,
-    np=numprocs,
-    rtol=reltol,
-    atol=abstol,
-    excluded_columns=["Maximum", "Minimum"]
-)
+if "coaxial/open" in cases
+    @info "Testing coaxial (open)..."
+    @time testcase(
+        "coaxial",
+        "coaxial_open.json",
+        "open";
+        palace=palace,
+        np=numprocs,
+        rtol=reltol,
+        atol=abstol,
+        excluded_columns=["Maximum", "Minimum"]
+    )
+end
 
-@info "Testing coaxial (matched)..."
-@time testcase(
-    "coaxial",
-    "coaxial_matched.json",
-    "matched";
-    palace=palace,
-    np=numprocs,
-    rtol=reltol,
-    atol=abstol,
-    excluded_columns=["Maximum", "Minimum"]
-)
+if "coaxial/matched" in cases
+    @info "Testing coaxial (matched)..."
+    @time testcase(
+        "coaxial",
+        "coaxial_matched.json",
+        "matched";
+        palace=palace,
+        np=numprocs,
+        rtol=reltol,
+        atol=abstol,
+        excluded_columns=["Maximum", "Minimum"]
+    )
+end
 
-@info "Testing CPW (lumped ports)"
-@time testcase(
-    "cpw",
-    "cpw_lumped_uniform.json",
-    "lumped_uniform";
-    palace=palace,
-    np=numprocs,
-    rtol=reltol,
-    atol=abstol,
-    excluded_columns=["Maximum", "Minimum"]
-)
+if "cpw/lumped_uniform" in cases
+    @info "Testing CPW (lumped ports)"
+    @time testcase(
+        "cpw",
+        "cpw_lumped_uniform.json",
+        "lumped_uniform";
+        palace=palace,
+        np=numprocs,
+        rtol=reltol,
+        atol=abstol,
+        excluded_columns=["Maximum", "Minimum"]
+    )
+end
 
-@info "Testing CPW (wave ports)"
-@time testcase(
-    "cpw",
-    "cpw_wave_uniform.json",
-    "wave_uniform";
-    palace=palace,
-    np=numprocs,
-    rtol=reltol,
-    atol=abstol,
-    excluded_columns=["Maximum", "Minimum"]
-)
+if "cpw/wave_uniform" in cases
+    @info "Testing CPW (wave ports)"
+    @time testcase(
+        "cpw",
+        "cpw_wave_uniform.json",
+        "wave_uniform";
+        palace=palace,
+        np=numprocs,
+        rtol=reltol,
+        atol=abstol,
+        excluded_columns=["Maximum", "Minimum"]
+    )
+end
 
 # Don't check accuracy for adaptive frequency sweep simulations
 
-@info "Testing CPW (lumped ports, adaptive)"
-@time testcase(
-    "cpw",
-    "cpw_lumped_adaptive.json",
-    "lumped_adaptive";
-    palace=palace,
-    np=numprocs,
-    rtol=Inf,
-    atol=Inf
-)
+if "cpw/lumped_adaptive" in cases
+    @info "Testing CPW (lumped ports, adaptive)"
+    @time testcase(
+        "cpw",
+        "cpw_lumped_adaptive.json",
+        "lumped_adaptive";
+        palace=palace,
+        np=numprocs,
+        rtol=Inf,
+        atol=Inf
+    )
+end
 
-@info "Testing CPW (wave ports, adaptive)"
-@time testcase(
-    "cpw",
-    "cpw_wave_adaptive.json",
-    "wave_adaptive";
-    palace=palace,
-    np=numprocs,
-    rtol=Inf,
-    atol=Inf
-)
+if "cpw/wave_adaptive" in cases
+    @info "Testing CPW (wave ports, adaptive)"
+    @time testcase(
+        "cpw",
+        "cpw_wave_adaptive.json",
+        "wave_adaptive";
+        palace=palace,
+        np=numprocs,
+        rtol=Inf,
+        atol=Inf
+    )
+end
