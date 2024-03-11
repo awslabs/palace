8fc26bce7b165ead45e1e0ad2a3b9adc8ee44557
Hugh Carson <hughcars@amazon.com>
2024-03-11 16:21:08 -0400

    Add in test for MixedVectorGradientIntegrator with H1 Hdiv

--- palace/fem/integrator.hpp
+++ palace/fem/integrator.hpp
@@ -14,6 +14,7 @@ class MaterialPropertyCoefficient;
 
 //
 // Classes which implement or extend bilinear and linear form integrators.
+// In doc strings u refers to the trial function, and v the test function.
 //
 
 namespace fem
