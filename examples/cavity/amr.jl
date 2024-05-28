b1d8d292a2ac0f2ab04e4ba52e7bd79946d4f343
simbilod <sb30@princeton.edu>
2024-05-28 16:16:16 -0700

    parse the json and csv for plotting data

--- examples/cavity/amr.jl
+++ examples/cavity/amr.jl
@@ -38,3 +38,94 @@ for (amr_nonconformal, output_dir) in
         dirname=output_dir
     )
 end
+
+# Compute the exact solution for reference
+~, f_TM_010_true = frequency_transverse(
+    0,
+    1,
+    0;
+    ϵᵣ   = 2.08,
+    μᵣ   = 1.0,
+    a_cm = radius,
+    d_cm = aspect_ratio * 2 * radius
+)
+f_TE_111_true, ~ = frequency_transverse(
+    1,
+    1,
+    1;
+    ϵᵣ   = 2.08,
+    μᵣ   = 1.0,
+    a_cm = radius,
+    d_cm = aspect_ratio * 2 * radius
+)
+
+# Parse the results
+# go through iterations to look for DOFs and compare to analytical answer
+# Loop through each subdirectory in the "amr" directory and process JSON and CSV files
+
+dof_conformal = Vector{Int}()
+f_TM_010_conformal = Vector{Float64}()
+f_TE_111_conformal = Vector{Float64}()
+err_norm_conformal = Vector{Float64}()
+
+dof_nonconformal = Vector{Int}()
+f_TM_010_nonconformal = Vector{Float64}()
+f_TE_111_nonconformal = Vector{Float64}()
+err_norm_nonconformal = Vector{Float64}()
+
+for (dof, f_TM_010, f_TE_111, err_norm, output_dir) in [
+    (
+        dof_conformal,
+        f_TM_010_conformal,
+        f_TE_111_conformal,
+        err_norm_conformal,
+        output_dir_conformal
+    ),
+    (
+        dof_nonconformal,
+        f_TM_010_nonconformal,
+        f_TE_111_nonconformal,
+        err_norm_nonconformal,
+        output_dir_nonconformal
+    )
+]
+    subdirectories = [filter(isdir, readdir(output_dir, join=true)); output_dir]
+
+    for subdir in subdirectories
+        # Read degrees of freedom from log
+        json_files = filter(f -> occursin(r"\.json$", f), readdir(subdir, join=true))
+        for json_file in json_files
+            json_data = JSON.parsefile(json_file)
+            push!(dof, json_data["Problem"]["DegreesOfFreedom"])
+        end
+        # Read frequencies from csv, and process
+        eig_files = filter(f -> occursin("eig.csv", f), readdir(subdir, join=true))
+        for eig_file in eig_files
+            eig_data = CSV.read(eig_file, DataFrame)
+            rename!(eig_data, strip.(names(eig_data)))
+            frequencies = Float64[]  # Temporary array to store frequencies for this file
+            for row in eachrow(eig_data)
+                push!(frequencies, row["Re{f} (GHz)"])
+            end
+            # The first dominant frequency should be the magnetic, and the second the
+            # electric, but just in case we search for the closest
+            push!(f_TM_010, frequencies[argmin(abs.(frequencies .- f_TM_010_true))])
+            push!(f_TE_111, frequencies[argmin(abs.(frequencies .- f_TE_111_true))])
+        end
+        # Read error markers from csv
+        error_files =
+            filter(f -> occursin("error-indicators.csv", f), readdir(subdir, join=true))
+        for error_file in error_files
+            error_data = CSV.read(error_file, DataFrame)
+            rename!(error_data, strip.(names(error_data)))
+            for row in eachrow(error_data)
+                push!(err_norm, row["Norm"])
+            end
+        end
+    end
+
+    println("Degrees of Freedom: ", dof)
+    println("f_TM_010: ", f_TM_010)
+    println("f_TE_111: ", f_TE_111)
+    println("err_norm: ", err_norm)
+end
