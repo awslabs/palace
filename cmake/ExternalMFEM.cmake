90d87649f04c43a494666d06ef443146ac4c9fb8
Sebastian Grimberg <sjg@amazon.com>
2024-05-31 12:26:28 -0700

    Dependency package updates

--- cmake/ExternalMFEM.cmake
+++ cmake/ExternalMFEM.cmake
@@ -357,7 +357,6 @@ set(MFEM_PATCH_FILES
   "${CMAKE_SOURCE_DIR}/extern/patch/mfem/patch_mfem_device_fixes.diff"
   "${CMAKE_SOURCE_DIR}/extern/patch/mfem/patch_mesh_vis_dev.diff"
   "${CMAKE_SOURCE_DIR}/extern/patch/mfem/patch_mesh_prism_vtu_fix.diff"
-  "${CMAKE_SOURCE_DIR}/extern/patch/mfem/patch_mesh_part_const.diff"
   "${CMAKE_SOURCE_DIR}/extern/patch/mfem/patch_par_tet_mesh_fix_dev.diff"
   "${CMAKE_SOURCE_DIR}/extern/patch/mfem/patch_gmsh_parser_performance.diff"
 )
