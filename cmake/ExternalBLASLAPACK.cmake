24ecb189d1ebf81b06297653dc64f36b9da3ebb9
Hugh Carson <hughcars@amazon.com>
2024-11-15 14:16:54 -0500

    Address PR feedback

--- cmake/ExternalBLASLAPACK.cmake
+++ cmake/ExternalBLASLAPACK.cmake
@@ -162,14 +162,13 @@ else()
     set(OPENBLAS_DIR $ENV{OPENBLAS_ROOT})
   else()
     set(OPENBLAS_DIR)
+    message(STATUS "Using BLAS/LAPACK located by CMake")
   endif()
 
   if(NOT OPENBLAS_DIR STREQUAL "")
     # If OpenBLAS was found set the vendor to avoid conflict with Accelerate on Darwin
     set(BLA_VENDOR "OpenBLAS")
     message(STATUS "Using BLAS/LAPACK from OpenBLAS")
-  else()
-    message(STATUS "Using BLAS/LAPACK located by CMake")
   endif()
 
   list(APPEND CMAKE_PREFIX_PATH ${OPENBLAS_DIR})
