bb2db8405ba257a4778ea514da9e704469df7713
Cameron Rutherford <rcamruzz@amazon.com>
2025-03-28 19:55:38 -0700

    Add scripts with working SPACK_ENV string...

--- spack/scripts/local-build.sh
+++ spack/scripts/local-build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [[ ! -z $SPACK_COMMAND ]]; then
+  echo "..."
+  echo
+else
+  SPACK_COMMAND=spack
+fi
+
 if [[ ! -f ${PWD}/spack/scripts/$(basename $0) ]]; then
   echo "Please run this script from the palace root dir."
   exit 1
@@ -18,9 +25,56 @@ if [ ! -f ${SPACK_ROOT}/share/spack/setup-env.sh ]; then
   git clone -c feature.manyFiles=true --depth=2 https://github.com/spack/spack.git $SPACK_ROOT
 fi
 
-source ${SPACK_ROOT}/share/spack/setup-env.sh || exit 1
+# Add optional bootstrap of Python if using ubuntu (in a container...)
+# This is made irrelevant by running with ubuntu/python instead...
+python3 --version >/dev/null 2>&1
+if [ ! $? -eq 0 ]; then
+  echo "Python not found!"
+  OS_VER=$(awk -F= '/^NAME/{print $2}' /etc/os-release)
+  echo "OS_VER == " ${OS_VER}
+  # The quotes are messy here and I don't like it
+  if [ "${OS_VER}" = '"Ubuntu"' ]; then
+    apt-get update
+    apt-get install -y python3
+    apt-get clean
+    rm -rfd /var/lib/apt/lists/*
+  else
+    exit 1
+  fi
+fi
 
-SPACK_ENV=${PWD}/$(spack arch --generic)
+# Add this optional bootstrap of gcc if using Ubuntu (in a container...)
+gcc --version >/dev/null 2>&1
+if [ ! $? -eq 0 ]; then
+  echo "GCC not found!"
+  OS_VER=$(awk -F= '/^NAME/{print $2}' /etc/os-release)
+  echo "OS_VER == " ${OS_VER}
+  # The quotes are messy here and I don't like it
+  if [ "${OS_VER}" = '"Debian GNU/Linux"' ]; then
+    apt-get update
+    apt-get install -y gcc g++ gfortran
+    apt-get clean
+    rm -rfd /var/lib/apt/lists/*
+  else
+    exit 1
+  fi
+
+fi
+
+if [[ ! -z $SPACK_COMMAND ]]; then
+  if [[ $SPACK_COMMAND == "spack" ]]; then
+    SPACK_PYTHON=$(which python3)
+    source ${SPACK_ROOT}/share/spack/setup-env.sh
+  fi
+fi
+
+# A quick check to make sure spack is working...
+echo Using spack@$(${SPACK_COMMAND} --version) || exit 1
+
+# Maybe this could be a one-liner...
+GARCH_STR="${SPACK_COMMAND} arch --generic"
+GARCH="$(${GARCH_STR})"
+SPACK_ENV=${PWD}/${GARCH}
 
 # Can easily change compiler / MPI / blas spec
 # NOTES:
@@ -28,7 +82,9 @@ SPACK_ENV=${PWD}/$(spack arch --generic)
 # PALACE_SPEC="local.palace@develop ^intel-oneapi-mkl ^openmpi"
 PALACE_SPEC="local.palace@develop ^openblas ^openmpi"
 
+# Somtimes we want to default to true
 FRESH_INSTALL=false
+# FRESH_INSTALL=true
 
 while [[ $# -gt 0 ]]; do
   case "$1" in
@@ -54,9 +110,6 @@ export TMPDIR=${TMP}
 export tempdir=${TMP}
 mkdir -p $TMP
 
-# Assumes that Python is installed / present on the system
-SPACK_PYTHON=$(which python3)
-
 # Should do a fresh install if no spack.yaml in env
 if [ ! -f ${SPACK_ENV}/spack.yaml ]; then
   FRESH_INSTALL=true
@@ -66,21 +119,21 @@ if [ ${FRESH_INSTALL} = "true" ]; then
   if [ -d ${SPACK_ENV} ]; then
     rm -rfd ${SPACK_ENV}
   fi
-  spack env create -d ${SPACK_ENV} || exit 1
+  ${SPACK_COMMAND} env create -d ${SPACK_ENV} || exit 1
 
   # We don't need to clean every time, but might as well to avoid issues...
-  # spack -e ${SPACK_ENV} clean -m
+  ${SPACK_COMMAND} -e ${SPACK_ENV} clean -m
 
   # Add our repo-local definition of palace spack package
-  spack -e ${SPACK_ENV} repo add spack/local
+  ${SPACK_COMMAND} -e ${SPACK_ENV} repo add spack/local
 
   # Add our package
-  spack -e ${SPACK_ENV} add ${PALACE_SPEC}
-  spack -e ${SPACK_ENV} develop --path=${PWD} ${PALACE_SPEC}
+  ${SPACK_COMMAND} -e ${SPACK_ENV} add ${PALACE_SPEC}
+  ${SPACK_COMMAND} -e ${SPACK_ENV} develop --path=${PWD} ${PALACE_SPEC}
 
   # Configure externals / concretizer
-  spack -e ${SPACK_ENV} compiler find
-  spack -e ${SPACK_ENV} external find
+  ${SPACK_COMMAND} -e ${SPACK_ENV} compiler find
+  ${SPACK_COMMAND} -e ${SPACK_ENV} external find --all
 
   __IS_MAC=${__IS_MAC:-$(test $(uname -s) == "Darwin" && echo 'true')}
 
@@ -96,40 +149,40 @@ if [ ${FRESH_INSTALL} = "true" ]; then
     )
     for PKG in "${PACKAGES[@]}"; do
       PKG_PATH=$(brew --prefix ${PKG})
-      spack -e ${SPACK_ENV} external find --path ${PKG_PATH} ${PKG}
+      ${SPACK_COMMAND} -e ${SPACK_ENV} external find --path ${PKG_PATH} ${PKG}
       # We could also add specification of virtual providers here...
-      spack -e ${SPACK_ENV} config add packages:${PKG}:buildable:false
+      ${SPACK_COMMAND} -e ${SPACK_ENV} config add packages:${PKG}:buildable:false
     done
   fi
 
   # Views are only for environments being used with many core specs
-  spack -e ${SPACK_ENV} config add view:false
+  ${SPACK_COMMAND} -e ${SPACK_ENV} config add view:false
   # We want to re-use externals/buildcache
   # However, the concretization kept giving new-deps, so disabling...
   # Maybe reuse:dependencies would be better
-  spack -e ${SPACK_ENV} config add concretizer:reuse:false
+  ${SPACK_COMMAND} -e ${SPACK_ENV} config add concretizer:reuse:false
   # Unify doesn't really apply until you have more than one core spec
-  spack -e ${SPACK_ENV} config add concretizer:unify:true
+  ${SPACK_COMMAND} -e ${SPACK_ENV} config add concretizer:unify:true
   # Allow splicing of MPI / Compilers from build cache for faster builds
   # While splicing does speed up builds, it often creates wild DAGs
-  spack -e ${SPACK_ENV} config add concretizer:splice:automatic:false
+  ${SPACK_COMMAND} -e ${SPACK_ENV} config add concretizer:splice:automatic:false
   # Try to prevent duplcate packages in concretization
-  spack -e ${SPACK_ENV} config add concretizer:duplicates:strategy:none
+  ${SPACK_COMMAND} -e ${SPACK_ENV} config add concretizer:duplicates:strategy:none
   # Target the most generic microarch to encourage re-use
-  spack -e ${SPACK_ENV} config add concretizer:targets:granularity:generic
-  # Lets make sure that spack uses our tmp directories
-  spack -e ${SPACK_ENV} config add config:source_cache:${TMP}
-  spack -e ${SPACK_ENV} config add config:misc_cache:${TMP}
-  spack -e ${SPACK_ENV} config add config:build_stage:${TMP}
+  ${SPACK_COMMAND} -e ${SPACK_ENV} config add concretizer:targets:granularity:generic
+  # Lets make sure that ${SPACK_COMMAND} uses our tmp directories
+  ${SPACK_COMMAND} -e ${SPACK_ENV} config add config:source_cache:${TMP}
+  ${SPACK_COMMAND} -e ${SPACK_ENV} config add config:misc_cache:${TMP}
+  ${SPACK_COMMAND} -e ${SPACK_ENV} config add config:build_stage:${TMP}
   # We have some requirements for other package variants
-  spack -e ${SPACK_ENV} config add packages:petsc:require:~hdf5
+  ${SPACK_COMMAND} -e ${SPACK_ENV} config add packages:petsc:require:~hdf5
 
   # Add public mirror to help with build times
-  spack -e ${SPACK_ENV} mirror add develop https://binaries.spack.io/develop
-  spack -e ${SPACK_ENV} buildcache keys --install --trust
+  ${SPACK_COMMAND} -e ${SPACK_ENV} mirror add develop https://binaries.spack.io/develop
+  ${SPACK_COMMAND} -e ${SPACK_ENV} buildcache keys --install --trust
 
   # Verify concretization is correct before installing
-  spack -e ${SPACK_ENV} concretize -f
+  ${SPACK_COMMAND} -e ${SPACK_ENV} concretize -f || exit 1
   echo ""
   echo "NOTE: Even though it doesn't say palace.local, it's using that version."
   echo "If you are happy with this concretization, press Enter to continue..."
@@ -138,9 +191,9 @@ if [ ${FRESH_INSTALL} = "true" ]; then
   echo ""
 fi
 
-spack -e ${SPACK_ENV} install -j $(nproc 2>/dev/null || sysctl -n hw.ncpu) --fail-fast --only-concrete
+${SPACK_COMMAND} -e ${SPACK_ENV} install -j $(nproc 2>/dev/null || sysctl -n hw.ncpu) --fail-fast --only-concrete
 
-DEV_PATH=$(spack -e ${SPACK_ENV} location --build-dir ${PALACE_SPEC})
+DEV_PATH=$(${SPACK_COMMAND} -e ${SPACK_ENV} location --build-dir ${PALACE_SPEC})
 
 echo
 echo "Installation done / cancelled / failed. Feel free to re-run build with:"
@@ -151,6 +204,7 @@ echo
 echo "OR re-run the script without -f as so:"
 echo "    ${PWD}/spack/scripts/$(basename $0)"
 echo
-echo "NOTE: The path in the cd command is also in ./build-$(spack arch)-<hash>"
+echo "NOTE: The path in the cd command is also in ./build-$(${SPACK_COMMAND} arch)-<hash>"
 echo "      which is symlinked to the one output above."
 echo
+echo "NOTE: Just re-run the script if you are using the container script"
--- spack/scripts/ubuntu-container-build.sh
+++ spack/scripts/ubuntu-container-build.sh
@@ -14,20 +14,50 @@ if [[ -z ${SPACK_ROOT} ]]; then
   exit 1
 fi
 
-UBUNTU_TAG=oracular
-FINCH_TEST="finch run --rm ubuntu:${UBUNTU_TAG} echo Finch runs Ubuntu..."
+# This is technically Debian and not ubuntu, but I didn't want to wait
+# for Python to install each time while debugging.
+CONTAINER=spack/ubuntu-noble
 CONTAINER_ROOT=/scratch
 CONTAINER_SPACK_ROOT=/opt/local-spack
 # Changing tmpdir requires changing local-build.sh as well.
 TMPDIR=/tmp/spack-tmp
 
-${FINCH_TEST} || (finch vm start && ${FINCH_TEST})
+finch vm start >/dev/null 2>&1 || true
 
+# This is our magic command that:
+#   - Runs spack in the ubuntu container
+#   - Using our local clone of spack
+#   - Storing files in the local spack install
+#
+# Since this literally only has spack bootstrapped, you can't run a container
+# directly. I wanted to iterate quickly, so I went with this.
+#
+# In the long term, devcontainers are nicer (once they work).
+#
+# To iterate quickly, and to cache well, these scripts are nice.
 finch run --rm \
   -v ${PWD}:${CONTAINER_ROOT} \
   -v ${SPACK_ROOT}:${CONTAINER_SPACK_ROOT} \
   -v ${TMPDIR}:${TMPDIR} \
   --env SPACK_ROOT=${CONTAINER_SPACK_ROOT} \
   -w ${CONTAINER_ROOT} \
-  ubuntu:${UBUNTU_TAG} \
-  ${CONTAINER_ROOT}/spack/scripts/local-build.sh
+  ${CONTAINER} \
+  "--version"
+
+SPACK_COMMAND="\
+finch run --rm \
+  -v ${PWD}:${CONTAINER_ROOT} \
+  -v ${SPACK_ROOT}:${CONTAINER_SPACK_ROOT} \
+  -v ${TMPDIR}:${TMPDIR} \
+  --env SPACK_ROOT=${CONTAINER_SPACK_ROOT} \
+  -w ${CONTAINER_ROOT} \
+  ${CONTAINER}"
+
+echo Using:
+echo
+echo $SPACK_COMMAND
+echo
+echo As the pseudo-spack command for the local build script.
+echo
+
+SPACK_COMMAND=$SPACK_COMMAND ./spack/scripts/local-build.sh
