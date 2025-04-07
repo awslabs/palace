9c73b2e8e3cb3b04bce0d19986a2a24e428326b3
Hugh Carson <hughcars@amazon.com>
2025-04-07 15:52:32 -0400

    Fix spack libceed build
    - Failure to detect CC vendor from `/usr/bin/cc`, patch to libCEED's
      makefile resolves

--- cmake/ExternalLibCEED.cmake
+++ cmake/ExternalLibCEED.cmake
@@ -121,6 +121,11 @@ endif()
 string(REPLACE ";" "; " LIBCEED_OPTIONS_PRINT "${LIBCEED_OPTIONS}")
 message(STATUS "LIBCEED_OPTIONS: ${LIBCEED_OPTIONS_PRINT}")
 
+# A number of patches to libCEED for our use cases
+set(LIBCEED_PATCH_FILES
+  "${CMAKE_SOURCE_DIR}/extern/patch/libCEED/patch_libceed_build.diff"
+)
+
 include(ExternalProject)
 ExternalProject_Add(libCEED
   DEPENDS           ${LIBCEED_DEPENDENCIES}
@@ -131,6 +136,10 @@ ExternalProject_Add(libCEED
   PREFIX            ${CMAKE_BINARY_DIR}/extern/libCEED-cmake
   BUILD_IN_SOURCE   TRUE
   UPDATE_COMMAND    ""
+  PATCH_COMMAND
+    git reset --hard &&
+    git clean -fd &&
+    git apply "${LIBCEED_PATCH_FILES}"
   CONFIGURE_COMMAND ""
   BUILD_COMMAND     ""
   INSTALL_COMMAND   ${CMAKE_MAKE_PROGRAM} ${LIBCEED_OPTIONS} install
