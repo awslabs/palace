af0c0cb07bfaec155c5bb0d64d2896f0c27ddf14
Philipp Dumitrescu <phdum@amazon.com>
2025-06-06 10:15:11 -0700

    Replace SFINAE with requires

--- palace/models/postoperator.cpp
+++ palace/models/postoperator.cpp
@@ -923,12 +923,11 @@ void PostOperator<solver_t>::MeasureProbes() const
 using fmt::format;
 
 template <config::ProblemData::Type solver_t>
-template <config::ProblemData::Type U>
-auto PostOperator<solver_t>::MeasureAndPrintAll(int ex_idx, int step,
-                                                const ComplexVector &e,
-                                                const ComplexVector &b,
-                                                std::complex<double> omega)
-    -> std::enable_if_t<U == config::ProblemData::Type::DRIVEN, double>
+double PostOperator<solver_t>::MeasureAndPrintAll(int ex_idx, int step,
+                                                  const ComplexVector &e,
+                                                  const ComplexVector &b,
+                                                  std::complex<double> omega)
+  requires(solver_t == config::ProblemData::Type::DRIVEN)
 {
   BlockTimer bt0(Timer::POSTPRO);
   auto freq = units.Dimensionalize<Units::ValueType::FREQUENCY>(omega);
@@ -958,13 +957,12 @@ auto PostOperator<solver_t>::MeasureAndPrintAll(int ex_idx, int step,
 }
 
 template <config::ProblemData::Type solver_t>
-template <config::ProblemData::Type U>
-auto PostOperator<solver_t>::MeasureAndPrintAll(int step, const ComplexVector &e,
-                                                const ComplexVector &b,
-                                                std::complex<double> omega,
-                                                double error_abs, double error_bkwd,
-                                                int num_conv)
-    -> std::enable_if_t<U == config::ProblemData::Type::EIGENMODE, double>
+double PostOperator<solver_t>::MeasureAndPrintAll(int step, const ComplexVector &e,
+                                                  const ComplexVector &b,
+                                                  std::complex<double> omega,
+                                                  double error_abs, double error_bkwd,
+                                                  int num_conv)
+  requires(solver_t == config::ProblemData::Type::EIGENMODE)
 {
   BlockTimer bt0(Timer::POSTPRO);
   auto freq = units.Dimensionalize<Units::ValueType::FREQUENCY>(omega);
@@ -985,7 +983,7 @@ auto PostOperator<solver_t>::MeasureAndPrintAll(int step, const ComplexVector &e
   {
     Table table;
     int idx_pad = 1 + static_cast<int>(std::log10(num_conv));
-    table.col_options = {6, 6};
+    table.col_options = {.min_left_padding = 6, .float_precision = 6};
     table.insert(Column("idx", "m", idx_pad, {}, {}, "") << step + 1);
     table.insert(Column("f_re", "Re{f} (GHz)") << freq.real());
     table.insert(Column("f_im", "Im{f} (GHz)") << freq.imag());
@@ -1011,10 +1009,9 @@ auto PostOperator<solver_t>::MeasureAndPrintAll(int step, const ComplexVector &e
 }
 
 template <config::ProblemData::Type solver_t>
-template <config::ProblemData::Type U>
-auto PostOperator<solver_t>::MeasureAndPrintAll(int step, const Vector &v, const Vector &e,
-                                                int idx)
-    -> std::enable_if_t<U == config::ProblemData::Type::ELECTROSTATIC, double>
+double PostOperator<solver_t>::MeasureAndPrintAll(int step, const Vector &v,
+                                                  const Vector &e, int idx)
+  requires(solver_t == config::ProblemData::Type::ELECTROSTATIC)
 {
   BlockTimer bt0(Timer::POSTPRO);
   SetVGridFunction(v);
@@ -1037,10 +1034,9 @@ auto PostOperator<solver_t>::MeasureAndPrintAll(int step, const Vector &v, const
   return total_energy;
 }
 template <config::ProblemData::Type solver_t>
-template <config::ProblemData::Type U>
-auto PostOperator<solver_t>::MeasureAndPrintAll(int step, const Vector &a, const Vector &b,
-                                                int idx)
-    -> std::enable_if_t<U == config::ProblemData::Type::MAGNETOSTATIC, double>
+double PostOperator<solver_t>::MeasureAndPrintAll(int step, const Vector &a,
+                                                  const Vector &b, int idx)
+  requires(solver_t == config::ProblemData::Type::MAGNETOSTATIC)
 {
   BlockTimer bt0(Timer::POSTPRO);
   SetAGridFunction(a);
@@ -1064,10 +1060,9 @@ auto PostOperator<solver_t>::MeasureAndPrintAll(int step, const Vector &a, const
 }
 
 template <config::ProblemData::Type solver_t>
-template <config::ProblemData::Type U>
-auto PostOperator<solver_t>::MeasureAndPrintAll(int step, const Vector &e, const Vector &b,
-                                                double t, double J_coef)
-    -> std::enable_if_t<U == config::ProblemData::Type::TRANSIENT, double>
+double PostOperator<solver_t>::MeasureAndPrintAll(int step, const Vector &e,
+                                                  const Vector &b, double t, double J_coef)
+  requires(solver_t == config::ProblemData::Type::TRANSIENT)
 {
   BlockTimer bt0(Timer::POSTPRO);
   auto time = units.Dimensionalize<Units::ValueType::TIME>(t);
@@ -1104,10 +1099,9 @@ void PostOperator<solver_t>::MeasureFinalize(const ErrorIndicator &indicator)
 }
 
 template <config::ProblemData::Type solver_t>
-template <config::ProblemData::Type U>
-auto PostOperator<solver_t>::MeasureDomainFieldEnergyOnly(const ComplexVector &e,
-                                                          const ComplexVector &b)
-    -> std::enable_if_t<U == config::ProblemData::Type::DRIVEN, double>
+double PostOperator<solver_t>::MeasureDomainFieldEnergyOnly(const ComplexVector &e,
+                                                            const ComplexVector &b)
+  requires(solver_t == config::ProblemData::Type::DRIVEN)
 {
   SetEGridFunction(e);
   SetBGridFunction(b);
@@ -1128,39 +1122,5 @@ template class PostOperator<config::ProblemData::Type::EIGENMODE>;
 template class PostOperator<config::ProblemData::Type::ELECTROSTATIC>;
 template class PostOperator<config::ProblemData::Type::MAGNETOSTATIC>;
 template class PostOperator<config::ProblemData::Type::TRANSIENT>;
-
-// Function explict instantiation.
-// TODO(C++20): with requires, we won't need a second template.
-
-template auto PostOperator<config::ProblemData::Type::DRIVEN>::MeasureAndPrintAll<
-    config::ProblemData::Type::DRIVEN>(int ex_idx, int step, const ComplexVector &e,
-                                       const ComplexVector &b, std::complex<double> omega)
-    -> double;
-
-template auto PostOperator<config::ProblemData::Type::EIGENMODE>::MeasureAndPrintAll<
-    config::ProblemData::Type::EIGENMODE>(int step, const ComplexVector &e,
-                                          const ComplexVector &b,
-                                          std::complex<double> omega, double error_abs,
-                                          double error_bkwd, int num_conv) -> double;
-
-template auto PostOperator<config::ProblemData::Type::ELECTROSTATIC>::MeasureAndPrintAll<
-    config::ProblemData::Type::ELECTROSTATIC>(int step, const Vector &v, const Vector &e,
-                                              int idx) -> double;
-
-template auto PostOperator<config::ProblemData::Type::MAGNETOSTATIC>::MeasureAndPrintAll<
-    config::ProblemData::Type::MAGNETOSTATIC>(int step, const Vector &a, const Vector &b,
-                                              int idx) -> double;
-
-template auto PostOperator<config::ProblemData::Type::TRANSIENT>::MeasureAndPrintAll<
-    config::ProblemData::Type::TRANSIENT>(int step, const Vector &e, const Vector &b,
-                                          double t, double J_coef) -> double;
-
-template auto PostOperator<config::ProblemData::Type::DRIVEN>::MeasureDomainFieldEnergyOnly<
-    config::ProblemData::Type::DRIVEN>(const ComplexVector &e, const ComplexVector &b)
-    -> double;
-
-template auto
-PostOperator<config::ProblemData::Type::DRIVEN>::InitializeParaviewDataCollection(
-    int ex_idx) -> void;
 
 }  // namespace palace
--- palace/models/postoperator.hpp
+++ palace/models/postoperator.hpp
@@ -59,35 +59,34 @@ using fem_op_t = typename fem_op_map_type<solver_t>::type;
 // Statically specify if solver uses real or complex fields.
 
 template <config::ProblemData::Type solver_t>
-constexpr bool HasComplexGridFunction()
+consteval bool HasComplexGridFunction()
 {
   return solver_t == config::ProblemData::Type::DRIVEN ||
          solver_t == config::ProblemData::Type::EIGENMODE;
 }
 
 // Statically specify what fields a solver uses
-// TODO(C++20): Change these to inline consteval and use with requires.
 
 template <config::ProblemData::Type solver_t>
-constexpr bool HasVGridFunction()
+consteval bool HasVGridFunction()
 {
   return solver_t == config::ProblemData::Type::ELECTROSTATIC;
 }
 
 template <config::ProblemData::Type solver_t>
-constexpr bool HasAGridFunction()
+consteval bool HasAGridFunction()
 {
   return solver_t == config::ProblemData::Type::MAGNETOSTATIC;
 }
 
 template <config::ProblemData::Type solver_t>
-constexpr bool HasEGridFunction()
+consteval bool HasEGridFunction()
 {
   return solver_t != config::ProblemData::Type::MAGNETOSTATIC;
 }
 
 template <config::ProblemData::Type solver_t>
-constexpr bool HasBGridFunction()
+consteval bool HasBGridFunction()
 {
   return solver_t != config::ProblemData::Type::ELECTROSTATIC;
 }
@@ -322,12 +321,9 @@ private:
   // These functions are private helper functions. We want to enforce that a caller passes
   // the appropriate ones as part of the MeasureAndPrintAll interface, rather than do a
   // runtime check to see that they have been set.
-  //
-  // TODO(C++20): Switch SFINAE to requires.
 
-  template <config::ProblemData::Type U = solver_t>
-  auto SetEGridFunction(const ComplexVector &e, bool exchange_face_nbr_data = true)
-      -> std::enable_if_t<HasEGridFunction<U>() && HasComplexGridFunction<U>(), void>
+  void SetEGridFunction(const ComplexVector &e, bool exchange_face_nbr_data = true)
+    requires(HasEGridFunction<solver_t>() && HasComplexGridFunction<solver_t>())
   {
     E->Real().SetFromTrueDofs(e.Real());  // Parallel distribute
     E->Imag().SetFromTrueDofs(e.Imag());
@@ -338,9 +334,8 @@ private:
     }
   }
 
-  template <config::ProblemData::Type U = solver_t>
-  auto SetEGridFunction(const Vector &e, bool exchange_face_nbr_data = true)
-      -> std::enable_if_t<HasEGridFunction<U>() && !HasComplexGridFunction<U>(), void>
+  void SetEGridFunction(const Vector &e, bool exchange_face_nbr_data = true)
+    requires(HasEGridFunction<solver_t>() && !HasComplexGridFunction<solver_t>())
   {
     E->Real().SetFromTrueDofs(e);
     if (exchange_face_nbr_data)
@@ -349,9 +344,8 @@ private:
     }
   }
 
-  template <config::ProblemData::Type U = solver_t>
-  auto SetBGridFunction(const ComplexVector &b, bool exchange_face_nbr_data = true)
-      -> std::enable_if_t<HasBGridFunction<U>() && HasComplexGridFunction<U>(), void>
+  void SetBGridFunction(const ComplexVector &b, bool exchange_face_nbr_data = true)
+    requires(HasBGridFunction<solver_t>() && HasComplexGridFunction<solver_t>())
   {
     B->Real().SetFromTrueDofs(b.Real());  // Parallel distribute
     B->Imag().SetFromTrueDofs(b.Imag());
@@ -362,9 +356,8 @@ private:
     }
   }
 
-  template <config::ProblemData::Type U = solver_t>
-  auto SetBGridFunction(const Vector &b, bool exchange_face_nbr_data = true)
-      -> std::enable_if_t<HasBGridFunction<U>() && !HasComplexGridFunction<U>(), void>
+  void SetBGridFunction(const Vector &b, bool exchange_face_nbr_data = true)
+    requires(HasBGridFunction<solver_t>() && !HasComplexGridFunction<solver_t>())
   {
     B->Real().SetFromTrueDofs(b);
     if (exchange_face_nbr_data)
@@ -373,9 +366,8 @@ private:
     }
   }
 
-  template <config::ProblemData::Type U = solver_t>
-  auto SetVGridFunction(const Vector &v, bool exchange_face_nbr_data = true)
-      -> std::enable_if_t<HasVGridFunction<U>() && !HasComplexGridFunction<U>(), void>
+  void SetVGridFunction(const Vector &v, bool exchange_face_nbr_data = true)
+    requires(HasVGridFunction<solver_t>() && !HasComplexGridFunction<solver_t>())
   {
     V->Real().SetFromTrueDofs(v);
     if (exchange_face_nbr_data)
@@ -384,9 +376,8 @@ private:
     }
   }
 
-  template <config::ProblemData::Type U = solver_t>
-  auto SetAGridFunction(const Vector &a, bool exchange_face_nbr_data = true)
-      -> std::enable_if_t<HasAGridFunction<U>() && !HasComplexGridFunction<U>(), void>
+  void SetAGridFunction(const Vector &a, bool exchange_face_nbr_data = true)
+    requires(HasAGridFunction<solver_t>() && !HasComplexGridFunction<solver_t>())
   {
     A->Real().SetFromTrueDofs(a);
     if (exchange_face_nbr_data)
@@ -415,34 +406,23 @@ public:
   // therefore).
   //
   // The measure functions will also do logging of (some) measurements to stdout.
-  //
-  // TODO(C++20): Upgrade SFINAE to C++20 concepts to simplify static selection since we can
-  // just write `MeasureAndPrintAll(...) requires (solver_t == Type::A)` without extra
-  // template.
+  double MeasureAndPrintAll(int step, const ComplexVector &e, const ComplexVector &b,
+                            std::complex<double> omega)
+    requires(solver_t == config::ProblemData::Type::DRIVEN);
 
-  template <config::ProblemData::Type U = solver_t>
-  auto MeasureAndPrintAll(int ex_idx, int step, const ComplexVector &e,
-                          const ComplexVector &b, std::complex<double> omega)
-      -> std::enable_if_t<U == config::ProblemData::Type::DRIVEN, double>;
+  double MeasureAndPrintAll(int ex_idx, int step, const ComplexVector &e,
+                            const ComplexVector &b, std::complex<double> omega)
+    requires(solver_t == config::ProblemData::Type::EIGENMODE);
 
-  template <config::ProblemData::Type U = solver_t>
-  auto MeasureAndPrintAll(int step, const ComplexVector &e, const ComplexVector &b,
-                          std::complex<double> omega, double error_abs, double error_bkwd,
-                          int num_conv)
-      -> std::enable_if_t<U == config::ProblemData::Type::EIGENMODE, double>;
+  double MeasureAndPrintAll(int step, const Vector &v, const Vector &e, int idx)
+    requires(solver_t == config::ProblemData::Type::ELECTROSTATIC);
 
-  template <config::ProblemData::Type U = solver_t>
-  auto MeasureAndPrintAll(int step, const Vector &v, const Vector &e, int idx)
-      -> std::enable_if_t<U == config::ProblemData::Type::ELECTROSTATIC, double>;
+  double MeasureAndPrintAll(int step, const Vector &a, const Vector &b, int idx)
+    requires(solver_t == config::ProblemData::Type::MAGNETOSTATIC);
 
-  template <config::ProblemData::Type U = solver_t>
-  auto MeasureAndPrintAll(int step, const Vector &a, const Vector &b, int idx)
-      -> std::enable_if_t<U == config::ProblemData::Type::MAGNETOSTATIC, double>;
-
-  template <config::ProblemData::Type U = solver_t>
-  auto MeasureAndPrintAll(int step, const Vector &e, const Vector &b, double t,
-                          double J_coef)
-      -> std::enable_if_t<U == config::ProblemData::Type::TRANSIENT, double>;
+  double MeasureAndPrintAll(int step, const Vector &e, const Vector &b, double t,
+                            double J_coef)
+    requires(solver_t == config::ProblemData::Type::TRANSIENT);
 
   // Write error indicator into ParaView file and print summary statistics to csv. Should be
   // called once at the end of the solver loop.
@@ -451,11 +431,8 @@ public:
   // Measurement of the domain energy without printing. This is needed during the driven
   // simulation with PROM. There samples are taken and we need the total domain energy for
   // the error indicator, but no other measurement / printing should be done.
-  //
-  // TODO(C++20): SFINAE to requires.
-  template <config::ProblemData::Type U = solver_t>
-  auto MeasureDomainFieldEnergyOnly(const ComplexVector &e, const ComplexVector &b)
-      -> std::enable_if_t<U == config::ProblemData::Type::DRIVEN, double>;
+  double MeasureDomainFieldEnergyOnly(const ComplexVector &e, const ComplexVector &b)
+    requires(solver_t == config::ProblemData::Type::DRIVEN);
 
   // Access grid functions for field solutions. Note that these are NOT const functions. The
   // electrostatics / magnetostatics solver do measurements of the capacitance/ inductance
@@ -464,28 +441,26 @@ public:
   //
   // Future: Consider moving those cap/ind measurements into this class and MeasureFinalize?
   // Would need to store vector of V,A.
-  //
-  // TODO(C++20): Switch SFINAE to requires.
-  template <config::ProblemData::Type U = solver_t>
-  auto GetEGridFunction() -> std::enable_if_t<HasEGridFunction<U>(), decltype(*E) &>
+  GridFunction &GetEGridFunction()
+    requires(HasEGridFunction<solver_t>())
   {
     return *E;
   }
 
-  template <config::ProblemData::Type U = solver_t>
-  auto GetBGridFunction() -> std::enable_if_t<HasBGridFunction<U>(), decltype(*B) &>
+  GridFunction &GetBGridFunction()
+    requires(HasBGridFunction<solver_t>())
   {
     return *B;
   }
 
-  template <config::ProblemData::Type U = solver_t>
-  auto GetVGridFunction() -> std::enable_if_t<HasVGridFunction<U>(), decltype(*V) &>
+  GridFunction &GetVGridFunction()
+    requires(HasVGridFunction<solver_t>())
   {
     return *V;
   }
 
-  template <config::ProblemData::Type U = solver_t>
-  auto GetAGridFunction() -> std::enable_if_t<HasAGridFunction<U>(), decltype(*A) &>
+  GridFunction &GetAGridFunction()
+    requires(HasAGridFunction<solver_t>())
   {
     return *A;
   }
