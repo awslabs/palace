6f5e8cd1ff78b2f594dcfb2110fa5250b9d9f656
Philipp Dumitrescu <phdum@amazon.com>
2025-06-20 16:42:48 -0700

    Allow restart data to be loaded into PostOperatorCSV
    - Must validate table structure against expectation
    - Must copy some parameter from reference that can't be parsed
    - Validate cursor location with restart
    - Properly set nr_expected_measurement_rows for all solvers

---
+++ test/unit/test-postoperatorcsv.cpp
@@ -0,0 +1,12 @@
+#include <complex>
+#include <fmt/format.h>
+#include <catch2/catch_approx.hpp>
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/benchmark/catch_benchmark_all.hpp>
+#include <catch2/generators/catch_generators_all.hpp>
+#include <catch2/matchers/catch_matchers_vector.hpp>
+#include "models/postoperatorcsv.hpp"
+
+using namespace palace;
+
+TEST_CASE("PostOperatorCSVCtor", "[postoperatorcsv]") {}
