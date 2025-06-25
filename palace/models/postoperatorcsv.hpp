92df44fe2435268142a8da8bdc4eb45893046936
Philipp Dumitrescu <phdum@amazon.com>
2025-06-25 11:29:17 -0700

    Remove post_op* in PostOperatorCSV for easier testing
    - Thread through objects needed for init and print in call

--- palace/models/postoperatorcsv.hpp
+++ palace/models/postoperatorcsv.hpp
@@ -4,6 +4,7 @@
 #include <memory>
 #include <optional>
 #include "fem/errorindicator.hpp"
+#include "models/spaceoperator.hpp"
 #include "utils/configfile.hpp"
 #include "utils/filesystem.hpp"
 #include "utils/tablecsv.hpp"
@@ -12,6 +13,11 @@
 namespace palace
 {
 class IoData;
+class DomainPostOperator;
+class SurfacePostOperator;
+class InterpolationOperator;
+class SurfaceCurrentOperator;
+class LumpedPortOperator;
 
 // Advance declaration.
 template <ProblemType solver_t>
@@ -125,7 +131,8 @@ struct Measurement
 template <ProblemType solver_t>
 class PostOperatorCSV
 {
-  PostOperator<solver_t> *post_op = nullptr;
+  // Copy savepath from PostOperator for simpler dependencies.
+  fs::path post_dir;
 
   // Dimensionalized measurement cache. Converted from the PostOperator member variable.
   Measurement measurement_cache;
@@ -159,72 +166,67 @@ class PostOperatorCSV
   // These are all std::optional since: (a) should only be instantiated on the root mpi
   // process, (b) they should only be written if the data is non-empty.
 
+  // Initialize and print methods for various output quantities: The initialize methods
+  // prepare the tables for data insertion, whilst the print methods insert data
+  // appropriately. Methods are only enabled when valid given the problem type.
+
   // Base (all solvers)
   std::optional<TableWithCSVFile> domain_E;
-  void InitializeDomainE();
+  void InitializeDomainE(const DomainPostOperator &dom_post_op);
   void PrintDomainE();
 
   std::optional<TableWithCSVFile> surface_F;
-  void InitializeSurfaceF();
+  void InitializeSurfaceF(const SurfacePostOperator &surf_post_op);
   void PrintSurfaceF();
 
   std::optional<TableWithCSVFile> surface_Q;
-  void InitializeSurfaceQ();
+  void InitializeSurfaceQ(const SurfacePostOperator &surf_post_op);
   void PrintSurfaceQ();
 
   std::optional<TableWithCSVFile> probe_E;
-  void InitializeProbeE();
-  void PrintProbeE();
+  void InitializeProbeE(const InterpolationOperator &interp_op);
+  void PrintProbeE(const InterpolationOperator &interp_op);
 
   std::optional<TableWithCSVFile> probe_B;
-  void InitializeProbeB();
-  void PrintProbeB();
+  void InitializeProbeB(const InterpolationOperator &interp_op);
+  void PrintProbeB(const InterpolationOperator &interp_op);
 
   // TODO: Upgrade SFINAE to C++20 concepts to simplify static selection since we can just
   // use `void Function(...) requires (solver_t == Type::A);`.
 
+  // Driven + Transient
+  std::optional<TableWithCSVFile> surface_I;
+  template <ProblemType U = solver_t>
+  auto InitializeSurfaceI(const SurfaceCurrentOperator &surf_j_op)
+      -> std::enable_if_t<U == ProblemType::DRIVEN || U == ProblemType::TRANSIENT, void>;
+  template <ProblemType U = solver_t>
+  auto PrintSurfaceI(const SurfaceCurrentOperator &surf_j_op, const Units &units)
+      -> std::enable_if_t<U == ProblemType::DRIVEN || U == ProblemType::TRANSIENT, void>;
+
   // Eigenmode + Driven + Transient
   std::optional<TableWithCSVFile> port_V;
   std::optional<TableWithCSVFile> port_I;
-
-  // Initialize and print methods for various output quantities. The initialize methods
-  // prepare the tables for data insertion, whilst the print methods insert data
-  // appropriately. Methods are only enabled when valid given the problem type.
-
   template <ProblemType U = solver_t>
-  auto InitializePortVI()
+  auto InitializePortVI(const SpaceOperator &fem_op)
       -> std::enable_if_t<U == ProblemType::EIGENMODE || U == ProblemType::DRIVEN ||
                               U == ProblemType::TRANSIENT,
                           void>;
-
   template <ProblemType U = solver_t>
-  auto PrintPortVI()
+  auto PrintPortVI(const LumpedPortOperator &lumped_port_op, const Units &units)
       -> std::enable_if_t<U == ProblemType::EIGENMODE || U == ProblemType::DRIVEN ||
                               U == ProblemType::TRANSIENT,
                           void>;
-
-  // Driven + Transient
-  std::optional<TableWithCSVFile> surface_I;
-
-  template <ProblemType U = solver_t>
-  auto InitializeSurfaceI()
-      -> std::enable_if_t<U == ProblemType::DRIVEN || U == ProblemType::TRANSIENT, void>;
-
-  template <ProblemType U = solver_t>
-  auto PrintSurfaceI()
-      -> std::enable_if_t<U == ProblemType::DRIVEN || U == ProblemType::TRANSIENT, void>;
 
   // Driven
   std::optional<TableWithCSVFile> port_S;
-
   template <ProblemType U = solver_t>
-  auto InitializePortS() -> std::enable_if_t<U == ProblemType::DRIVEN, void>;
+  auto InitializePortS(const SpaceOperator &fem_op)
+      -> std::enable_if_t<U == ProblemType::DRIVEN, void>;
   template <ProblemType U = solver_t>
   auto PrintPortS() -> std::enable_if_t<U == ProblemType::DRIVEN, void>;
 
   // Eigenmode
   std::optional<TableWithCSVFile> eig;
-
   template <ProblemType U = solver_t>
   auto InitializeEig() -> std::enable_if_t<U == ProblemType::EIGENMODE, void>;
   template <ProblemType U = solver_t>
@@ -233,45 +235,45 @@ class PostOperatorCSV
   std::vector<int> ports_with_L;
   std::vector<int> ports_with_R;
   std::optional<TableWithCSVFile> port_EPR;
-
   template <ProblemType U = solver_t>
-  auto InitializeEigPortEPR() -> std::enable_if_t<U == ProblemType::EIGENMODE, void>;
+  auto InitializeEigPortEPR(const LumpedPortOperator &lumped_port_op)
+      -> std::enable_if_t<U == ProblemType::EIGENMODE, void>;
   template <ProblemType U = solver_t>
   auto PrintEigPortEPR() -> std::enable_if_t<U == ProblemType::EIGENMODE, void>;
 
   std::optional<TableWithCSVFile> port_Q;
-
   template <ProblemType U = solver_t>
-  auto InitializeEigPortQ() -> std::enable_if_t<U == ProblemType::EIGENMODE, void>;
+  auto InitializeEigPortQ(const LumpedPortOperator &lumped_port_op)
+      -> std::enable_if_t<U == ProblemType::EIGENMODE, void>;
   template <ProblemType U = solver_t>
   auto PrintEigPortQ() -> std::enable_if_t<U == ProblemType::EIGENMODE, void>;
 
   // Set-up all files to be called from post_op.
-  void InitializeCSVDataCollection();
+  void InitializeCSVDataCollection(const PostOperator<solver_t> &post_op);
 
 public:
   // Print all data from nondim_measurement_cache.
-  void PrintAllCSVData(const Measurement &nondim_measurement_cache,
+  void PrintAllCSVData(const PostOperator<solver_t> &post_op,
+                       const Measurement &nondim_measurement_cache,
                        double idx_value_dimensionful, int step);
 
   // Driven specific overload for specifying excitation index
   template <ProblemType U = solver_t>
-  auto PrintAllCSVData(const Measurement &nondim_measurement_cache,
+  auto PrintAllCSVData(const PostOperator<solver_t> &post_op,
+                       const Measurement &nondim_measurement_cache,
                        double idx_value_dimensionful, int step, int ex_idx)
       -> std::enable_if_t<U == ProblemType::DRIVEN, void>
   {
     m_ex_idx = ex_idx;
-    PrintAllCSVData(nondim_measurement_cache, idx_value_dimensionful, step);
+    PrintAllCSVData(post_op, nondim_measurement_cache, idx_value_dimensionful, step);
   }
 
   // Special case of global indicator — init and print all at once.
-  void PrintErrorIndicator(const ErrorIndicator::SummaryStatistics &indicator_stats);
+  void PrintErrorIndicator(bool is_root,
+                           const ErrorIndicator::SummaryStatistics &indicator_stats);
 
   // "Delayed ctor" so that PostOperator can call it once it is fully constructed.
-  void SetUpAndInitialize(const IoData &iodata);
-
-  PostOperatorCSV() = delete;
-  explicit PostOperatorCSV(PostOperator<solver_t> *post_op_) : post_op(post_op_) {};
+  void SetUpAndInitialize(const IoData &iodata, const PostOperator<solver_t> &post_op);
 };
 
 }  // namespace palace
