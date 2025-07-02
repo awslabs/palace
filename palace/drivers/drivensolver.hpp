4d4ef4bfe33b43ded43edb48612864b71ee48c3b
Hugh Carson <hughcars@amazon.com>
2025-07-02 19:07:20 -0400

    Remove EnumerateView in favour of modular arithmetic

--- palace/drivers/drivensolver.hpp
+++ palace/drivers/drivensolver.hpp
@@ -18,27 +18,6 @@ template <ProblemType>
 class PostOperator;
 class SpaceOperator;
 
-// Mini helper class that stored indexing information for printing and restart.
-struct DrivenSolverIndexing
-{
-  std::size_t nr_total_samples;
-
-  // Restart (1-Based indexing from config file)
-  std::size_t restart = 1;
-
-  // Offsets from restart (0-Based)
-  std::size_t excitation_n0 = 0;
-  std::size_t omega_n0 = 0;
-
-  DrivenSolverIndexing(std::size_t nr_port_excitations, std::size_t nr_freq_samples,
-                       std::size_t restart_)
-    : nr_total_samples(nr_port_excitations * nr_freq_samples), restart(restart_),
-      excitation_n0(std::size_t(restart - 1) / nr_freq_samples),
-      omega_n0(std::size_t(restart - 1) % nr_freq_samples)
-  {
-  }
-};
-
 //
 // Driver class for driven terminal simulations.
 //
