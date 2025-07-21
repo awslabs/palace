847bacf0c3be09beaf96d0cc5baa0fca859828d9
Gabriele Bozzola <gbozzola@amazon.com>
2025-07-21 14:33:19 -0700

    Add Quick Start tutorial
    This commit adds a "Quick Start" tutorial that walks new users through
    their first simulation by providing a different view on the "two sphere"
    example. This tutorial is partially run and populated when the
    documentation is generated, so I updated the `docs.yml` GitHub action.
    
    This commit also adds comments to the `examples/spheres/mesh.jl` file,
    with the hope to be more accessible to users with no prior gmsh
    experience (complementing the tutorial, which does not cover meshing).

--- docs/make.jl
+++ docs/make.jl
@@ -19,6 +19,7 @@ makedocs(
     authors="Sebastian Grimberg, sjg@amazon.com",
     pages=[
         "Home" => "index.md",
+        "Quick Start" => "quick.md",
         "install.md",
         "run.md",
         "User Guide" => Any[
