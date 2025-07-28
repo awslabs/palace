be7753d4de21f316929bded7dd00cd8fe729d82f
Gabriele Bozzola <gbozzola@amazon.com>
2025-07-28 15:57:15 -0700

    Add dipole antenna example

--- docs/make.jl
+++ docs/make.jl
@@ -38,6 +38,7 @@ makedocs(
             "examples/examples.md",
             "examples/spheres.md",
             "examples/rings.md",
+            "examples/antenna.md",
             "examples/cylinder.md",
             "examples/coaxial.md",
             "examples/cpw.md"
