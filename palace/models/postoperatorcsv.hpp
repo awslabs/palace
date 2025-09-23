82759d7501111777acdedb119bf7c38a3e23f434
Hugh Carson <hughcars@amazon.com>
2025-09-23 16:39:22 -0400

    Fix missing cpp licenses

--- palace/models/postoperatorcsv.hpp
+++ palace/models/postoperatorcsv.hpp
@@ -1,3 +1,6 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 #ifndef PALACE_MODELS_POST_OPERATOR_CSV_HPP
 #define PALACE_MODELS_POST_OPERATOR_CSV_HPP
 
