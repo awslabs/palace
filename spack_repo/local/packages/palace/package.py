1242736c561a76727540f3d0276f5a9bb9254eac
Gabriele Bozzola <gbozzola@amazon.com>
2026-03-09 10:24:45 -0700

    Update Spack package to 0.16
    And remove patch that was merged

--- spack_repo/local/packages/palace/package.py
+++ spack_repo/local/packages/palace/package.py
@@ -21,6 +21,7 @@ class Palace(CMakePackage, CudaPackage, ROCmPackage):
     maintainers("hughcars", "simlap", "cameronrutherford", "sbozzolo", "phdum")
 
     version("develop", branch="main")
+    version("0.16.0", tag="v0.16.0", commit="869ee5ced4850384410a7aeebc7c25f4c01be161")
     version("0.15.0", tag="v0.15.0", commit="b6762777d85a06072fdf4cc96e8a365da73df170")
     version("0.14.0", tag="v0.14.0", commit="a428a3a32dbbd6a2a6013b3b577016c3e9425abc")
     version("0.13.0", tag="v0.13.0", commit="a61c8cbe0cacf496cde3c62e93085fae0d6299ac")
