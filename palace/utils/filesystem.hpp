28d1bc81c361a3594f5f5dd6f659924b9fcc1d55
Niko Savola <nikomsavola@gmail.com>
2026-09-18 15:52:35 +0300

    Drop the dead pre-C++17 filesystem fallback

--- palace/utils/filesystem.hpp
+++ palace/utils/filesystem.hpp
@@ -4,21 +4,10 @@
 #ifndef PALACE_UTILS_FILESYSTEM_HPP
 #define PALACE_UTILS_FILESYSTEM_HPP
 
-#if defined(__cpp_lib_filesystem) || defined(__has_include) && __has_include(<filesystem>)
 #include <filesystem>
 namespace palace
 {
 namespace fs = std::filesystem;
 }  // namespace palace
-#elif defined(__cpp_lib_experimental_filesystem) || \
-    defined(__has_include) && __has_include(<experimental/filesystem>)
-#include <experimental/filesystem>
-namespace palace
-{
-namespace fs = std::experimental::filesystem;
-}  // namespace palace
-#else
-#error "Could not find system header <filesystem> or <experimental/filesystem>!"
-#endif
 
 #endif  // PALACE_UTILS_FILESYSTEM_HPP
