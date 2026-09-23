174e553d091d5302d4ba17d419d1eb55b839edfd
Dzung Pham <dzpham@amazon.com>
2026-09-23 08:23:13 -0700

    spack: loosen mfem pin to @4.10: so develop-deps lookahead concretizes

--- spack_repo/local/packages/palace/package.py
+++ spack_repo/local/packages/palace/package.py
@@ -203,7 +203,7 @@ class Palace(CMakePackage, CudaPackage, ROCmPackage):
     with when("@0.16:"):
         # +lapack means: use external lapack
         depends_on(
-            "mfem+mpi+metis+lapack@4.10.0",
+            "mfem+mpi+metis+lapack@4.10:",
             patches=[
                 # https://github.com/mfem/mfem/pull/3847
                 patch(
